f2ebfc54648c761b5a9e5298720bc4a9438d3c8e
alanmore231 <52770776+alanmore231@users.noreply.github.com>
2021-08-12 15:17:23 +0800

    Create SumbyCellColor.vb

---
+++ SumbyCellColor.vb
@@ -0,0 +1,16 @@
+'Sum by cell color, a function written in 2015
+Function ColorSum(Ref_color As Range, Sum_range As Range) 
+
+    Application.Volatile
+    Dim iCol As Integer
+    Dim rCell As Range
+
+    iCol = Ref_color.Interior.ColorIndex
+    
+    For Each rCell In Sum_range
+        If iCol = rCell.Interior.ColorIndex Then
+            ColorSum = ColorSum + rCell.Value
+        End If
+    Next rCell
+    
+End Function
